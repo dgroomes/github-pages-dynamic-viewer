1fb464e6fb36aa17e5beda958938bd2c7ba12071
David Groomes <dwgroomes@gmail.com>
2020-10-31 21:53:28 -0500

    De-React the creation of 'li' tags

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 'use strict';
 
-window.elementIdCounter = 1;
-window.untetheredElement = null;
+window.elementIdCounter = 1
+window.untetheredElement = null
+window.untetheredElements = []
 window.postReactElementCreationCallbacks = []
 
 /**
@@ -24,17 +25,40 @@ window.postReactElementCreationCallbacks = []
  * there must be some global way I could hook into React to wire in my own frameworky code, but I don't have enough time
  * to research that right now.
  *
- * Cases supported:
- * - For requests to create anchor tags (`<a>`), the DOM API `Document.createElement` will be used
- *   in a callback that gets executed after the React application lifecycle finished
+ * Cases supported: anchor tags (`<a>`), list item tags (`<li`)
  */
 function myCreateElement(tagName, options, ...otherArgs) {
     if (tagName === 'a') {
-        console.log("Creating an element ('a') *without* React.")
+        let content = otherArgs[0];
+        let href = options.href;
+        console.log(`Creating an element ('a') *without* React. content='${content} href=${href}'`)
         let el = document.createElement('a')
-        el.href = options.href;
-        el.innerHTML = otherArgs[0];
+        el.href = href;
+        el.innerHTML = content;
         window.untetheredElement = el;
+        return;
+    }
+
+    // Now we're getting more complicated than when we just had to deal with the 'a' tag. The 'a' element is a
+    // "leaf node" and will not have elements inside of it (at least, in the case of this app). But, the 'li' element
+    // will have elements inside of it (again, in the case of this app) so we have to coordinate the creation and
+    // appending of the 'li' element's child elements with the creation and appending of the 'li' element itself. I'm
+    // getting confused!
+    //
+    // How does the lifecycle work? Well, when the execution gets here, we have already created the 'a' element and
+    // referenced it via the 'window.untetheredElement' variable. So, we have to attach the 'a' element and nullify the
+    // window.untetheredElement reference. Now, the 'a' element does not need processing anymore and will be carried to
+    // the DOM via it's parent 'li' element. It's hitching a free ride! Additionally we have another challenge: managing
+    // how to attach a *collection* of untethered elements. After all, 'li' elements are list items and so we have to
+    // attach many of them to a common shared parent element. The singular 'window.untetheredElement' mechanism won't
+    // accommodate this. So, let's introduce a 'window.untetheredElements' array variable.
+    if (tagName === 'li') {
+        console.log("Creating an element ('li') *without* React.")
+        let el = document.createElement('li');
+        let child = window.untetheredElement;
+        window.untetheredElement = null;
+        el.appendChild(child);
+        window.untetheredElements.push(el)
         return;
     }
 
@@ -44,19 +68,34 @@ function myCreateElement(tagName, options, ...otherArgs) {
     let elementId = `globally-unique-id-${window.elementIdCounter++}`;
     console.log(`Creating an element ('${tagName}') using React. Assigning id: '${elementId}'`)
     options['id'] = elementId;
+
+    // Meld a singular untethered element into the array of untethered elements. I'm not sure this is necessary or will
+    // ever be useful for my toy app because of the limited range of elements on the page but for a larger app it might
+    // be...
     let untetheredElement = window.untetheredElement;
     if (untetheredElement != null) {
-        console.log("There is an untethered element. It should probably be tethered to the element that is being created now. So, we will register a callback to push the untethered element to this element *after* React is done doing it's thing.")
+        window.untetheredElements.push(untetheredElement)
+        window.untetheredElement = null
+    }
+    let untetheredElements = window.untetheredElements
+    window.untetheredElements = []
+    if (untetheredElements.length > 0) {
+        console.log("Registering a callback to push the untethered elements to this element *after* React is done doing it's thing.")
         window.postReactElementCreationCallbacks.push(() => {
+            console.log(`Attaching untethered elements (${untetheredElements.length})`)
             let reactCreatedElement = document.getElementById(elementId)
             if (reactCreatedElement == null) {
                 console.log(`Something went wrong. Did not find an element for id ${elementId}. So, the untethered element will remain untethered (sad).`)
                 return;
             }
+
             reactCreatedElement.innerHTML = ''; // the inner HTML often already contains content (and I don't totally know why, look at the logs) so clear it.
-            reactCreatedElement.appendChild(untetheredElement);
+            while (untetheredElements.length > 0) {
+                let untetheredElement = untetheredElements.pop();
+                console.log(`Attaching untethered element '${untetheredElement.tagName}'`)
+                reactCreatedElement.appendChild(untetheredElement);
+            }
         })
-        window.untetheredElement = null;
     }
     return React.createElement(tagName, options, ...otherArgs)
 }
